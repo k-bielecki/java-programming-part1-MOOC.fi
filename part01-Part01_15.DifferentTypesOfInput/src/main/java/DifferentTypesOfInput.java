3d057e6ebd8ceac8eb5be2ec1099b01a0e0f905a
Krzysztof Bielecki <145564055+k-bielecki@users.noreply.github.com>
2024-06-05 22:39:52 +0200

    Add files via upload

---
+++ part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -0,0 +1,26 @@
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class DifferentTypesOfInput {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give a string:");
+        String string = scan.nextLine();
+        System.out.println("Give an integer:");
+        int integer = Integer.valueOf(scan.nextLine());
+        System.out.println("Give a double:");
+        double multi = Double.valueOf(scan.nextLine());
+        System.out.println("Give a boolean:");
+        boolean trueorfalse = Boolean.valueOf(scan.nextLine());
+
+        System.out.println("You gave the string " + string);
+        System.out.println("You gave the integer " + integer);
+        System.out.println("You gave the double " + multi);
+        System.out.println("You gave the boolean " + trueorfalse);
+
+
+    }
+}
