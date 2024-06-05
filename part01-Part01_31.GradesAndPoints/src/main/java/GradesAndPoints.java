ac9972a0df005593365da3027cdf4de1e9c2e46e
Krzysztof Bielecki <145564055+k-bielecki@users.noreply.github.com>
2024-06-05 22:41:52 +0200

    Add files via upload

---
+++ part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class GradesAndPoints {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give points [0-100]:");
+        int number = scan.nextInt();
+
+        if (number < 0) {
+            System.out.println("impossible!");
+        } else if (number < 50) {
+            System.out.println("Grade: failed");
+        } else if (number < 60) {
+            System.out.println("Grade: 1");
+        } else if (number < 70) {
+            System.out.println("Grade: 2");
+        } else if (number < 80) {
+            System.out.println("Grade: 3");
+        } else if (number < 90) {
+            System.out.println("Grade: 4");
+        } else if (number <= 100) {
+            System.out.println("Grade: 5");
+        } else {
+            System.out.println("incredible!");
+        }
+
+    }
+}
